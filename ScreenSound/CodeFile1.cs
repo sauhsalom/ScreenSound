1e99646446ee41054e945412056c07ae8f84340d
sauh <sauhgonsalves@hotmail.com>
2024-05-15 11:08:10 -0300

    Add project files.

---
+++ ScreenSound/CodeFile1.cs
@@ -0,0 +1,26 @@
+﻿
+class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida =>
+        $"A música {Nome} pertence a banda {Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus.");
+        }
+    }
+}
+
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Album albumDoQuenn = new Album();
+albumDoQuenn.Nome = "A night at the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
+
+albumDoQuenn.AdicionarMusica(musica1);
+albumDoQuenn.AdicionarMusica(musica2);
+
+albumDoQuenn.ExibirMusicasDoAlbum();
